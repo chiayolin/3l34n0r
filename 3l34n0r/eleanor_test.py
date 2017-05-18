70d6117f2b5e7535fd469dab68b599f4bfc4eb33
chiayolin <chiayo.lin@gmail.com>
2017-05-18 17:17:02 +0700

    add eleanor prototype

---
+++ 3l34n0r/eleanor_test.py
@@ -0,0 +1,27 @@
+from pytelegram.telegram import *
+
+from modules.greet import greet
+from modules.eliza import eliza
+from modules.today import today
+from modules.email import email
+
+def run_command(command, args = []):
+    return command(*args)
+
+def callback(bot, message, chat_id):
+    case = message.split()[0]
+
+    print(case)
+    
+    if   case == '/greet' : func = greet
+    elif case == '/today' : func = today
+    elif case == '/email' : func = send_email
+    else                  : func = eliza
+
+    response = run_command(func, [bot, message, chat_id])
+    bot.sendMessage(chat_id, response)
+
+# -- main --
+if __name__ == '__main__':
+    token = '347436566:AAEUK5kLnP7dgxhmKnAn57DfG_rCZD-KgQo'
+    Telegram(token).listen(callback)
