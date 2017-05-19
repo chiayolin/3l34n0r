bbd835e36b4ed4dbafd8e31f38d077712eadec16
chiayolin <chiayo.lin@gmail.com>
2017-05-19 23:07:50 +0700

    update docstrings

--- 3l34n0r/server.py
+++ 3l34n0r/server.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Server for the 3l34n0r bot"""
+"""The server for 3l34n0r bot"""
 
 from telegram import Telegram
 from modules.chat import eliza
