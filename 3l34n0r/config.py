89597c4341430dc90318b51072f9c1014e86cee6
chiayolin <chiayo.lin@gmail.com>
2017-05-20 06:54:31 +0700

    rename config

--- 3l34n0r/config.py
+++ 3l34n0r/config.py
@@ -16,3 +16,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os.path
+
+# get path and load environment
+os.path.join(os.path.dirname(__file__), '3l34n0r')
