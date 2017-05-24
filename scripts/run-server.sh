fc7e661ea0d25c230d7e6ba52687b01127211d94
chiayolin <chiayo.lin@gmail.com>
2017-05-24 04:22:05 +0000

    update server script: IMCOMPLETE

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,1 +1,21 @@
-python3 ../eleanor/server.py --noauth_local_webserver
+SERVER="../eleanor/server.py"
+ARGS="--noauth_local_webserver"
+RUN="python3"
+
+
+PROCESS="$RUN $SERVER $ARGS"
+
+
+function find_process
+
+PROCESSCOUNT=$(ps -ef |grep -v grep |grep -cw <daemon process name>
+
+if [$PROCESSCOUNT -eq 0]
+then
+  mailx -s "daemon process not running" myaddress@mydomain <msg body
+
+fi
+
+# double fork to detach from the current TTY
+($RUN $SERVER $ARGS </dev/null >/dev/null 2>/dev/null &) &
+exit 0
