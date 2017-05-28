4f5cdd546f59e3e70d4238cac607301ff74cabbc
Chiayo Lin <chiayo.lin@gmail.com>
2017-05-28 11:40:55 +0700

    update one more command

--- eleanor/modules/start.py
+++ eleanor/modules/start.py
@@ -20,4 +20,5 @@
 def start(bot, message, chat_id):
     return "Greetings!1 I'm Eleanor, your virtual personal assistant. Feel " + \
            "free to talk to me or control me by sending these commands:\n\n" + \
-           "/start - print this message\n/today - show events for today"
+           "/start - print this message\n/today - show events for today\n"   + \
+           "/gmail - send an email via Gmail's SMTP"
